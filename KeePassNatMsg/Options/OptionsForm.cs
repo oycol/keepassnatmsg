2f2f50d19b57e2945ea78f46db6f426e0660a346
oycol <oycol@users.noreply.github.com>
2026-09-24 22:51:40 +0800

    fix(ui): add missing System.Drawing namespace for Size/Point/SystemIcons

--- KeePassNatMsg/Options/OptionsForm.cs
+++ KeePassNatMsg/Options/OptionsForm.cs
@@ -2,6 +2,7 @@
 using KeePassNatMsg.NativeMessaging;
 using KeePassNatMsg.Utils;
 using System;
+using System.Drawing;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
